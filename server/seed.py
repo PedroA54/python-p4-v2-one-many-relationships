d5e5873ef89fe445ad831b779f28286379b4c7bd
Pedro Alarcon <pedroa54@icloud.com>
2024-05-14 00:10:43 -0500

    completes lesson

--- server/seed.py
+++ server/seed.py
@@ -6,6 +6,7 @@ from app import app
 from models import db, Employee, Review, Onboarding
 
 with app.app_context():
+
     # Delete all rows in tables
     Employee.query.delete()
     Review.query.delete()
@@ -30,10 +31,15 @@ with app.app_context():
     )
     db.session.add_all([uri_2023, tristan_2021, tristan_2022, tristan_2023])
     db.session.commit()
+
     # 1..1 relationship between Employee and Onboarding
-    uri_onboarding = Onboarding(orientation=datetime.datetime(2023, 3, 27))
+    uri_onboarding = Onboarding(
+        orientation=datetime.datetime(2023, 3, 27), employee=uri
+    )
     tristan_onboarding = Onboarding(
-        orientation=datetime.datetime(2020, 1, 20, 14, 30), forms_complete=True
+        orientation=datetime.datetime(2020, 1, 20, 14, 30),
+        forms_complete=True,
+        employee=tristan,
     )
     db.session.add_all([uri_onboarding, tristan_onboarding])
     db.session.commit()
